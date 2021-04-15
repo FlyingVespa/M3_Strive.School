ffabb110d29b932850ecbc5ef16eac6d6b9bd766
Helena Nel <mailhedri@gmail.com>
2021-04-15 15:37:47 +0200

    comits

--- M3D3/main.js
+++ M3D3/main.js
@@ -1,69 +1,46 @@
-const main = document.querySelector("main");
-let arrURL = []
-
-function loadImg() {
-
-    fetch("http://www.splashbase.co/api/v1/images/search?query=black")
-    .then((response) => response.json())
-    .then((data) => {
-        //Select
-        let row = document.querySelector(".album div.row");
-        let container = document.querySelector("div.album div.container");
-        var body = document.getElementsByTagName("body");
-        
-        //Create
-        let newRow = document.createElement("div");
-      
-        arrURL = data.images.map((urls) => urls.url);
-        console.log(data);
-
-        body.innerHTML= `Total: ${data.images.length}`;
-        main.classList.add("d-block")
-        row.remove();
-        newRow.classList.add("row");
-        container.appendChild(newRow);
-        
-        for (let i = 0; i < body.images.length; i++) {
-            let card
-
-            card = document.createElement("div");
-            card.classList.add("col-md-4");
-            card.classList.add("bg-dark");
-            card.setAttribute("id", `img${i}`);
-            card.innerHTML = `<div class="card mb-3">
-                <img src="${body.images[i].url}"/>
-                <div class="card-body">
-                <p class="card-text">
-                    Some Text is here to fill gab
-                </p>
-                <div
-                    class="d-flex justify-content-between align-items-center"
-                >
-                    <div class="btn-group">
-                    <button
-                        type="button"
-                        class="btn btn-sm btn-outline-secondary"
-                        data-toggle="modal" data-target="#exampleModal"
-                    >
-                        View
-                    </button>
-                    <button
-                        type="button"
-                        class="btn btn-sm btn-outline-secondary"
-                        onclick="hideImage(this)"
-                    >
-                        Hide
-                    </button>
-                    </div>
-                    <small class="text-muted">${body.images[i].id}</small>
-                </div>
-                </div>
-            </div>
-            
-       `;
-        newRow.appendChild(card);
-        }
-    });
+function loadImg ({images}) {
+    const cards = document.querySelectorAll(".card");
+    for (let i = 0; i < cards.length; i++) {
+        let card = cards[i];
+        card.firstElementChild.remove();
+        let img = document.createElement("img");
+        img.style = "height: 225px";
+        img.className = "card-img";
+        img.src = images[i].url;
+        card.innerHTML = img.outerHTML + " " + card.innerHTML ;
+    }
 };
 
-window.onload = ()=> loadImg();
+const loadImages = () => {
+    fetch("http://www.splashbase.co/api/v1/images/search?query=black")
+      .then((response) => response.json())
+      .then(loadImg)
+      .catch((error) => console.log(error));
+}
+
+// let input = document.querySelector(input.value)
+
+function addS () {
+    const hideBtn = document.querySelector("body > main > div > div > div > div > div > div > div > div > button:nth-child(2)")
+    for (let i = 0; i < hideBtn.length; i++) {
+        hideBtn[i].setAttribute("id","myHideBtn");
+    }
+}
+
+document.getElementById("myBtn").addEventListener("click", hideCard);
+
+
+// function hideCard() {
+//     let card = document.querySelector(".card")
+//     for (let i = 0; i < card.length; i++) {
+//       card.classList.add("visually-hidden")
+   
+// }
+
+}
+window.onload = () => addS()
+
+let showAll = document.querySelector(".show-all");
+showAll.addEventListener("click", () => {
+  game.cardsArray.forEach((card) => card.classList.add("visible"));
+});
---
+++ M3D4/main.js
@@ -0,0 +1,74 @@
+window.onload = () => {
+  fetch("https://striveschool-api.herokuapp.com/books")
+    .then((res) => res.json())
+    .then((books) => {
+      console.log(books);
+
+      const row = document.createElement("div");
+      row.classList.add("row");
+      row.classList.add("no-gutter");
+
+      books.forEach((book) => {
+        let div = document.createElement("div");
+        div.classList.add("col-md-4");
+        div.innerHTML = `
+          <div class="card" style="width: 18rem;">
+          <img src="${book.img}" class="card-img-top" alt="...">
+          <div class="card-body">
+          <h5 class="card-title">${book.title}</h5>
+          <h5 class="card-title">${book.price}</h5>
+          <h5 class="card-title">${book.catagory}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-success" id="cartBtn">ADD TO CART</a>
+          <a href="#" class="btn btn-primary" id= "skipBtn">Skip</a>
+          </div>
+          </div>
+         
+          `;
+        row.appendChild(div);
+        // row.innerHTML = div.outerHTML + " " + div.innerHTML;
+      });
+    });
+};
+
+// const searchBar = `
+// <input class = "form-control type="text" width=400 required >
+// <input type="button" value="Search">
+// `;
+
+// function skipItem() {
+//   const skipBtn = document.querySelectorAll("#skipBtn");
+
+//   for (let i = 0; i < skipBtn.length; i++) {
+//     const element = array[i];
+//     element.classList.toggle("hidden");
+//   }
+// }
+// function changeStyle() {
+//   const cartBtn = document.querySelectorAll("#cartBtn");
+
+//   for (let i = 0; i < cartBtn.length; i++) {
+//     const element = array[i];
+//     element.classList.toggle("hidden");
+//   }
+// };
+
+// let shoppingCartList = [];
+
+// function emptyShoppingCart() {
+//   shoppingCartList.innerHTML = "";
+// }
+
+// // SEARACH FUNCTION
+// function searchBook(e) {
+//   const text = e.target.value.toLowerCase();
+//   const books = document.querySelectorAll(".book");
+//   for (let book of books) {
+//     const item = book.textContent;
+//     if (item.toLowerCase().indexOf(text) != -1) {
+//       task.style.display = "flex";
+//     } else {
+//       task.style.display = "none";
+//     }
+//   }
+//
