a119bfc03eac0175e4c95b581227a41f159c2bd9
Helena Nel <mailhedri@gmail.com>
2021-04-22 05:09:57 +0200

    commit

--- M3D8/js/main.js
+++ M3D8/js/main.js
@@ -4,14 +4,19 @@ const productName = document.getElementById("product-name").value;
 const productPrice = document.getElementById("product-price").value;
 const productImg = document.getElementById("product-img").value;
 const productBrand = document.getElementById("product-brand").value;
-const productDesc = document.getElementById("product-desc").value;
+// const productDesc = document.getElementById("product-desc").value;
+const products = document.getElementById("products");
+const product = document.getElementById("product");
 
 window.onload = async () => {
   console.log("written first, but printed when?");
 
   const url = "https://striveschool-api.herokuapp.com/api/product/";
+  //   const products = document.getElementById("products");
 
   try {
+    // prevents from total crashing - try
+    // async & await method
     let resp = await fetch(url, {
       method: "GET",
       headers: {
@@ -21,15 +26,55 @@ window.onload = async () => {
     });
     console.log("this is written second, printed 2nd?");
     console.log(resp);
+    let json = await resp.json();
+    console.log(json); // where is my json data :( ????????
+
+    if (json.length > 0) {
+      json.forEach((item) => {
+        product.innerHTML = `
+        <div class="card" >
+        <img
+            src=${item.url}
+            class="card-img-top"
+            alt="${item.name}"
+        />
+        <div class="card-body">
+            <div class="text-center">
+            <h5 class="card-title" id="item-name">${item.name}</h5>
+            <h6 id="item-brand">${item.brand}</h6>
+            </div>
+            <p class="card-text" id="item-desc">
+            ${item.desc}
+            </p>
+            <h6 class="text-center" id="item-price">$${item.price}</h6>
+            <a href="#" class="btn btn-primary w-100">Add To Cart</a>
+        </div>
+        </div>    `;
+        products.appendChild(product);
+      });
+    } else {
+      products.innerHTML = "<h1>No products were added to page</h1>";
+    }
   } catch (error) {
     console.log(error);
-    let json = await resp.json();
     console.log("this is written 3rd, printed 3rd?");
-    console.log(json); // where is my json data :( ????????
   }
 };
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(e);
 
-// change textarea count8
+  const item = {
+    name: productName,
+    brand: productBrand,
+    price: productPrice,
+    desc: productDesc,
+    url: productImg,
+  };
+  console.log(item);
+};
+
+// change textarea count
 var textarea = document.querySelector("textarea");
 const char = document.getElementById("char");
 
